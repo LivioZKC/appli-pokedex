7399507063b7ef774ce4b4a88498bb2cfb403aa5
Livio Zorkic <liviozorkic@MacBook-Pro-de-Livio.local>
2022-05-18 07:14:10 +0200

    Maj fichier

---
+++ Utilis/StorageService.js
@@ -0,0 +1,32 @@
+/*
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const storeData = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+    } catch (error){
+        console.log("error storeData", error)
+    }
+}
+
+export const retrieveData = async (key, value) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        if (value !== null){
+
+            return value
+        } else {
+            return null
+        }
+    }catch (error){
+        console.log("error retrieveData", error)
+    }
+}
+
+export const eraseData = async (key, value) => {
+    try {
+        await AsyncStorage.removeItem(key)
+    } catch (error){
+        console.log("error eraseData", error)
+    }
+}*/
