1643e7d8b4148fdac6dcad4d6243856cdb2d801f
Livio Zorkic <liviozorkic@MacBook-Pro-de-Livio.local>
2022-05-18 07:02:37 +0200

    MAJ PokeApi.js

--- Api/PokeApi.js
+++ Api/PokeApi.js
@@ -1,5 +1,13 @@
-export function getPokemons(url="https://pokeapi.co/api/v2/pokemon"){
+function getPokemons(url="https://pokeapi.co/api/v2/pokemon"){
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.log('error', error));
 }
+
+function getPokemonByName(name) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response)=> response.json())
+        .catch((error) => undefined);
+}
+
+export { getPokemons, getPokemonByName };
